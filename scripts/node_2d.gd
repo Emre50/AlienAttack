beac7650c9e98e605d7ea5ba84821d3a95cc99c6
Emre Deveci <devecie03@gmail.com>
2024-08-03 23:29:09 +0300

    Update
    Health bar added

--- scripts/node_2d.gd
+++ scripts/node_2d.gd
@@ -7,11 +7,13 @@ var score = 0
 @onready var ui = $ui
 @onready var enemy_hit_sound = $EnemyHitSound
 @onready var player_damage = $PlayerDamage
+@onready var health_bar = $HealthBar
 var game_over_screen = preload("res://scenes/game_over_screen.tscn")
 
 func _ready():
 	hud.score_label(score)
 	hud.set_lives(lives)
+	health_bar.value = lives
 
 func _on_death_zone_area_entered(area):
 	area.die()
@@ -20,6 +22,7 @@ func _on_death_zone_area_entered(area):
 
 func _on_player_damage():
 	lives-=1
+	health_bar.value = lives
 	score -=100
 	hud.score_label(score)
 	hud.set_lives(lives)
